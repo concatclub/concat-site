2247d7d53a7df53729e5813cae3b4c6ccb82f3fe
Jem (Jam) Bezooyen <hey@jem.dev>
2021-01-27 21:05:44 -0800

    feat(land-acknowledgement): Add link in footer
    The aknowledgement is important enough to keep on every page, so I'm
    putting this into the footer for now

--- components/PageFooter.jsx
+++ components/PageFooter.jsx
@@ -1,8 +1,11 @@
 import SiteNavigation from './SiteNavigation'
+import Link from 'next/link'
 
 const PageFooter = () => (
   <footer>
     <SiteNavigation />
+    <br />
+    <Link href="/land-acknowledgement" />
 
     <style jsx>{`
       footer { margin-top: 4rem; }
