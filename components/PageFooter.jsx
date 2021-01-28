821c068d3992ed21d738f3222caa589e9cfcc068
j'em bones [he/them] <hey@jem.dev>
2021-01-27 21:13:13 -0800

    feat(acknowledgement): Add Land Acknowledgement
    * Add Land Acknowledgement (for Victoria)
    
    I want the meetup and associated pages to be welcoming and inclusive, so
    starting with a land-acknowledgement feels like a good base/bare
    minimum.
    
    - Add the content for the page
    - Include a link in the footer
    - One quick style change for footer: will need to be revisited asap

--- components/PageFooter.jsx
+++ components/PageFooter.jsx
@@ -1,11 +1,14 @@
 import SiteNavigation from './SiteNavigation'
+import Link from 'next/link'
 
 const PageFooter = () => (
   <footer>
     <SiteNavigation />
+    <br />
+    <Link href="/land-acknowledgement">Land Acknowledgement</Link>
 
     <style jsx>{`
-      footer { margin-top: 4rem; }
+      footer { margin-top: 4rem; text-align: right; }
     `}</style>
   </footer>
 )
